ac05803ea0caa381dd19e1b169bd1ee85a4e12b5
Jose Ortega <josesantanas111110@gmail.com>
2023-07-17 10:56:03 -0700

    primer commit

---
+++ src/main/java/clases/operacion.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package clases;
+
+/**
+ *
+ * @author Juan Ortega
+ */
+public class operacion {
+    
+    public int s;
+    
+    public void sumar(int a, int b){
+         s = a + b;
+    }
+    
+    public int r;
+    public void restar (int a, int b) {
+        r= a-b;
+    }
+    public int m;
+    public void multiplicar (int a, int b) {
+        m= a*b;
+    }
+    public int d;
+    public void dividir (int a, int b) {
+        d= a/b;
+    }
+    
+}
